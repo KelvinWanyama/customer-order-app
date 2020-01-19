2e1962f6975cb508bc72993bcfcfe40ca0c0787e
KelvinWanyama <kevmurumba@gmail.com>
2020-01-19 10:22:29 +0300

    Getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: `
+    <h1>{{title}}</h1>
+  `
 })
-export class AppComponent {
-  title = 'customer-order-app';
+export class AppComponent implements OnInit {
+  title: string;
+  constructor(){}
+
+  ngOnInit(){
+    this.title = 'Hello World'
+  }
 }
