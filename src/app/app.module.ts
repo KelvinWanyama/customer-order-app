d11944eb219819b65deeaa8a62d9c8e2c4238ced
KelvinWanyama <kevmurumba@gmail.com>
2020-01-22 21:46:54 +0300

    Customer component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomersModule } from './customers/customers.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ AppComponent ],
+  imports: [  BrowserModule, AppRoutingModule, CustomersModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
